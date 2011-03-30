17d0544719795823f4132eee8eaac2f151d91a02
Bryan Mishkin <bryan.mishkin@gmail.com>
2011-03-30 14:00:47 -0500

    api specification is now html5 valid

--- api/query_helpers.php
+++ api/query_helpers.php
@@ -5,7 +5,7 @@ function showHelp($error)
 	$title = 'CourseOverflow API Specification';
 	
 	echo '<!DOCTYPE HTML><html><head><title>'.$title.'</title><meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />';
-	echo '<style type="text/css">table{width:100%;} th{width:25%;text-align:left;} td,th{padding-left:6px; padding-right:6px; vertical-align:top} #pagetitle{text-align:center} .error{color:red; font-weight:bold}</style>';
+	echo '<style type="text/css">table{width:800px;} th{width:25%;text-align:left;} td,th{padding-left:6px; padding-right:6px; vertical-align:top} #pagetitle{text-align:center} .error{color:red; font-weight:bold} #desc {padding-left:6px}</style>';
 	echo '</head><body>';
 	
 	if(!empty($error)){
@@ -14,14 +14,16 @@ function showHelp($error)
 	
 	echo '<div id="pagetitle"><h3>'.$title.'</h3></div>';
 	
+	echo '<p id="desc">Plug your newsgroup reader into our API to add support for voting, reputations, and remembering user subscriptions.</p>';
+	
 	echo '<table>';
 	echo '<tr><th>action</th><th>return</th><th>param1</th><th>param2</th></tr>';
-	echo '<tr><td>authenticate</td><td>boolean</td><td>email</td><td>password</td></tr>';
-	echo '<tr><td>submit_vote</td><td>boolean</td><td>message_id</td><td>&nbsp;</td></tr>';
+	echo '<tr><td>authenticate</td><td>bool</td><td>email</td><td>password</td></tr>';
+	echo '<tr><td>submit_vote</td><td>bool</td><td>message_id</td><td>&nbsp;</td></tr>';
 	echo '<tr><td>get_votes</td><td>array - list of votes on a post</td><td>message_id</td><td>&nbsp;</td></tr>';
 	echo '<tr><td>get_reputation</td><td>int - reputation of a user</td><td>email</td><td>&nbsp;</td></tr>';
-	echo '<tr><td>subscribe_to_class</td><td>boolean</td><td>class_name</td><td>&nbsp;</td></tr>';
-	echo '<tr><td>unsubscribe_from_class</td><td>boolean</td><td>class_name</td><td>&nbsp;</td></tr>';
+	echo '<tr><td>subscribe_to_class</td><td>bool</td><td>fully_qualified_class_name</td><td>&nbsp;</td></tr>';
+	echo '<tr><td>unsubscribe_from_class</td><td>bool</td><td>fully_qualified_class_name</td><td>&nbsp;</td></tr>';
 	echo '<tr><td>get_subscriptions</td><td>array - list of groups that a user is subscribed to</td><td>email</td><td>&nbsp;</td></tr>';
 	echo '</table>';
 	
