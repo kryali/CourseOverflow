10a9f6a6a6291480559379a6ca618f005cd13038
Joe Kaczmarek <jkaczma2@illinois.edu>
2011-03-29 20:07:56 -0500

    Fixed functions

--- api/functions.php
+++ api/functions.php
@@ -1,32 +1,32 @@
 <?php
 
-function authenticate(email, password){
-    return 0;
+function authenticate($email, $password){
+    return false;
 }
 
-function submit_vote(message_id){
-
+function submit_vote($message_id){
+    return false;
 }
 
-function get_votes(message_id){
-    $ret[0][0] = 0;
+function get_votes($message_id){
+    $ret = array();
     return $ret;
 }
 
-function get_reputation(email_address){
+function get_reputation($email_address){
     return 0;
 }
 
-function subscribe_to_class(class_name){
-
+function subscribe_to_class($class_name){
+    return false;
 }
 
-function unsubscribe_from_class(class_name){
-
+function unsubscribe_from_class($class_name){
+    return false;
 }
 
-function get_subscriptions(email_address){
-    $ret[0][0] = 0;
+function get_subscriptions($email_address){
+    $ret = array();
     return $ret;
 }
 
