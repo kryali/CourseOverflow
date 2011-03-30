c1dce89d4f4f76fcbe509db6fc7acda4798ca64f
Joe Kaczmarek <jkaczma2@illinois.edu>
2011-03-29 20:55:17 -0500

    Added SQL schema

---
+++ api/api-schema.sql
@@ -0,0 +1,17 @@
+-- Drop old versions
+--DROP TABLE Votes;
+--DROP TABLE Users;
+
+-- Schema
+CREATE TABLE Votes(
+    email      varchar(100) not null,
+    message_id varchar(100) not null,
+    positive   boolean not null default true,
+    primary key (email, message_id)
+);
+
+CREATE TABLE Users(
+    email varchar(100) not null,
+    reputation integer not null default 0,
+    primary key (email)
+);
