2b5f9b91714dd549f9a83fb60a591aa2bccb2a02
Joe Kaczmarek <jkaczma2@illinois.edu>
2011-03-29 19:44:37 -0500

    Added definitions

---
+++ api/functions.php
@@ -0,0 +1,32 @@
+<?php
+
+function authenticate(email, password){
+    return 0;
+}
+
+function submit_vote(message_id){
+
+}
+
+function get_votes(message_id){
+    return 0;
+}
+
+function get_reputation(email_address){
+    return 0;
+}
+
+function subscribe_to_class(class_name){
+
+}
+
+function unsubscribe_from_class(class_name){
+
+}
+
+function get_subscriptions(email_address){
+    $ret[0][0] = 0;
+    return $ret;
+}
+
+?>
