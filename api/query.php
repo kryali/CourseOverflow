ef4f8d7cb4895599e0abbff760f697eb65c84469
Bryan Mishkin <bryan.mishkin@gmail.com>
2011-03-29 21:03:09 -0500

    Added api specification to query.php

---
+++ api/query.php
@@ -0,0 +1,65 @@
+<?php
+require_once("functions.php");
+require_once("query_helpers.php");
+
+// GET variables
+$action = cleanInput($_GET['action']);
+
+if(empty($action)){
+	showError("No action provided.");
+	showHelp();
+	exit;
+}
+
+if($action != "authenticated" && !isset($_SESSION['auth'])){
+	showError("Not authenticated.");
+	showHelp();
+	exit;
+}
+
+if($action == "authenticate"){
+	
+	$email = cleanInput($_GET['email']);
+	$password = cleanInput($_GET['password']);
+	$ret = authenticate($email,$password);
+	
+	outputBoolean("authenticate",$ret);
+	
+}else if($action == "submit_vote"){
+
+	$message_id = cleanInput($_GET['message_id']);
+	$ret = submit_vote($message_id);
+	outputBoolean("submit_vote",$ret);
+
+}else if($action == "get_votes"){
+	
+	$message_id = cleanInput($_GET['message_id']);
+	$votes = get_votes($message_id);
+	outputResults("get_votes",$votes);
+
+}else if($action == "get_reputation"){
+
+	$email = cleanInput($_GET['email']);
+	$reputation = get_reputation($email);
+	outputResults("get_reputation",$reputation);
+
+}else if($action == "subscribe_to_class"){
+
+	$class_name = cleanInput($_GET['class_name']);
+	$ret = subscribe_to_class($class_name);
+	outputBoolean("subscribe_to_class",$ret);
+
+}else if($action == "unsubscribe_from_class"){
+	
+	$class_name = cleanInput($_GET['class_name']);
+	$ret = unsubscribe_from_class($class_name);
+	outputBoolean("unsubscribe_from_class",$ret);
+
+}else if($action == "get_subscriptions"){
+	
+	$email = cleanInput($_GET['email']);
+	$subscriptions = get_subscriptions($email);
+	outputResults("get_subscriptions",$subscriptions);
+}
+
+?>
