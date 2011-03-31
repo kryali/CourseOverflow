89854b6ffc4b934f18989edf65ec252885ab34c3
Joe Kaczmarek <jkaczma2@illinois.edu>
2011-03-31 12:42:01 -0500

    Added vote submission implementation

--- api/functions.php
+++ api/functions.php
@@ -1,11 +1,70 @@
 <?php
 
+include("db_connect.php");
+
 function authenticate($email, $password){
-    return false;
+    //TODO Authenticate with newsgroup
+
+    session_start();
+    $_SESSION["email"] = $email;    
+    
+    return true;
 }
 
-function submit_vote($message_id){
-    return false;
+function submit_vote($message_id, $positive){
+
+    if(!isset($_SESSION["email"]))
+        return false;
+
+    //Check for duplicate votes
+    $query  = "SELECT * FROM Votes WHERE ";
+    $query .= "email = '" . mysql_real_escape_string($_SESSION["email"]) . "' AND ";
+    $query .= "message_id = '" . mysql_real_escape_string($message_id) . "'";
+    $query .= ";";
+
+    $result = mysql_query($query);
+
+    if(!$result)
+        return false;
+
+    $res_array = mysql_fetch_assoc($result);
+
+    //Delete existing votes
+    if(mysql_num_rows($result) != 0){
+
+        if($positive && $res_array["positive"] == 1)
+            return false;
+        else if(!$positive && $res_array["positive"] == 0)
+            return false;
+
+        $query  = "DELETE FROM Votes WHERE ";
+        $query .= "email = '" . mysql_real_escape_string($_SESSION["email"]) . "' AND ";
+        $query .= "message_id = '" . mysql_real_escape_string($message_id) . "'";
+        $query .= ";";
+        
+        $result = mysql_query($query);
+
+        if(!$result)
+            return false;
+    }
+
+    //Insert the new vote
+    $query  = "INSERT INTO Votes Values( ";
+    $query .= "'" . mysql_real_escape_string($_SESSION["email"]) . "', ";
+    $query .= "'" . mysql_real_escape_string($message_id) . "', ";
+
+    if($positive)
+        $query .= "1";
+    else
+        $query .= "0";
+
+    $query .= ");";
+
+    $result = mysql_query($query);
+    if(!$result)
+        return false;
+
+    return true;
 }
 
 function get_votes($message_id){
