36b82ac3d3be5d99e38be9aa4b387f7c1b432715
Bryan Mishkin <bryan.mishkin@gmail.com>
2011-04-02 16:36:53 -0500

    added submit button

--- html4nntp/test.php
+++ html4nntp/test.php
@@ -1,17 +1,10 @@
-<?php
-require("html4nntp/nntp.php");
-require("config/html4nntp.cfg.php");
-
-$username = $_POST['username'];
-$password = $_POST['password'];
-
-?>
-
-<form method="get" action="http://courseoverflow.web.cs.illinois.edu/CourseOverflow/api/">
+<form method="get" action="http://courseoverflow.web.cs.illinois.edu/CourseOverflow/api/query.php">
 
 <input type="hidden" name="action" value="authenticate" />
 
 <p>Email: <input type="email" name="username" id="username" /></p>
 <p>Password: <input type="password" name="password" id="password" /></p>
+
+<p><input type="submit"></p>
 
 </form>
