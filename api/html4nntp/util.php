a416867f228cf25f7d3773350f9bebb31d193db3
Bryan Mishkin <bryan.mishkin@gmail.com>
2011-04-03 16:02:32 -0500

    fixing subscriptions

--- api/html4nntp/util.php
+++ api/html4nntp/util.php
@@ -359,6 +359,7 @@ function get_author($message_id) {
 	$username = $_SESSION["username"];
 	$password = $_SESSION["password"];
 
+	print "<p>username is $username</p>";
 	if (strlen($username) > 0) {	// Won't allow empty user name
 		// Create a dummy connection for authentication
 		$nntp = new NNTP($nntp_server, $username, $password, $proxy_server, $proxy_port, $proxy_user, $proxy_pass);
