d2ccb4062cfa474af789d908b8cae85b8b0f60a5
Bryan Mishkin <bryan.mishkin@gmail.com>
2011-04-05 13:02:39 -0500

    adding vote icon

--- html4nntp/html4nntp/show_header.php
+++ html4nntp/html4nntp/show_header.php
@@ -492,9 +492,9 @@
 			echo "<span class=\"msg-tree\"><a name=\"".$message_info->nntp_message_id."\" ></a>";
 			echo "$old_indent$sign<img src=\"".$image_base."message.gif\" width=\"13\" height=\"13\" alt=\"#\" /></span>";
 			if ((isset($_REQUEST["article_id"])) && ($_REQUEST["article_id"]==$message_info->nntp_message_id)) {
-				echo "<span class=\"msg-title\"><img src="" height=\"10px\" width=\"10px\" title=\"Thumbs Up!\" /> <strong>(+".$voteCount.")".htmlentities(chop_str($message_info->subject, $subject_length_limit - $level*3))."</strong></span>\r\n";
+				echo "<span class=\"msg-title\"><img src=\"thumbsup.jpeg\" height=\"10px\" width=\"10px\" title=\"Thumbs Up!\" /> <strong>(+".$voteCount.")".htmlentities(chop_str($message_info->subject, $subject_length_limit - $level*3))."</strong></span>\r\n";
 			} else {
-				echo "<span class=\"msg-title\"><img src="" height=\"10px\" width=\"10px\" title=\"Thumbs Up!\" /> (+".$voteCount.") <a href=\"".basename($_SERVER["SCRIPT_NAME"])."?art_group=".urlencode($_SESSION["newsgroup"])."&amp;article_id=".$message_info->nntp_message_id."\">";
+				echo "<span class=\"msg-title\"><img src=\"thumbsup.jpeg\" height=\"10px\" width=\"10px\" title=\"Thumbs Up!\" /> (+".$voteCount.") <a href=\"".basename($_SERVER["SCRIPT_NAME"])."?art_group=".urlencode($_SESSION["newsgroup"])."&amp;article_id=".$message_info->nntp_message_id."\">";
 				echo htmlentities(chop_str($message_info->subject, $subject_length_limit - $level*3))."</a></span>\r\n";
 			}
 			echo "</td>\r\n";
