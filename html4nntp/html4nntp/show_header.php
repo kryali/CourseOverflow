fcbaf5274a8329285e8c08957b3b9ce8c4668030
Bryan Mishkin <bryan.mishkin@gmail.com>
2011-04-05 14:25:45 -0500

    fixed problem with  and making vote button into link is in process

--- html4nntp/html4nntp/show_header.php
+++ html4nntp/html4nntp/show_header.php
@@ -509,9 +509,9 @@
 			echo "<span class=\"msg-tree\"><a name=\"".$message_info->nntp_message_id."\" ></a>";
 			echo "$old_indent$sign<img src=\"".$image_base."message.gif\" width=\"13\" height=\"13\" alt=\"#\" /></span>";
 			if ((isset($_REQUEST["article_id"])) && ($_REQUEST["article_id"]==$message_info->nntp_message_id)) {
-				echo "<span class=\"msg-title\">".$voteIconHTML." <strong>(+".$voteCount.") ".htmlentities(chop_str($message_info->subject, $subject_length_limit - $level*3))."</strong></span>\r\n";
+				echo "<div id=\"image_".$message_id."\">".$voteIconHTML."</div><span class=\"msg-title\"> <strong>(+".$voteCount.") ".htmlentities(chop_str($message_info->subject, $subject_length_limit - $level*3))."</strong></span>\r\n";
 			} else {
-				echo "<span class=\"msg-title\">".$voteIconHTML." (+<span id=\"count_".$message_id."\">".$voteCount."</span>) <a href=\"".basename($_SERVER["SCRIPT_NAME"])."?art_group=".urlencode($_SESSION["newsgroup"])."&amp;article_id=".$message_info->nntp_message_id."\">";
+				echo "<div id=\"image_".$message_id."\">".$voteIconHTML."</div><span class=\"msg-title\"> (+<span id=\"count_".$message_id."\">".$voteCount."</span>) <a href=\"".basename($_SERVER["SCRIPT_NAME"])."?art_group=".urlencode($_SESSION["newsgroup"])."&amp;article_id=".$message_info->nntp_message_id."\">";
 				echo htmlentities(chop_str($message_info->subject, $subject_length_limit - $level*3))."</a></span>\r\n";
 			}
 			echo "</td>\r\n";
